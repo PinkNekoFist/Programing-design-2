2cf489e0507b23091d57e70952e6c3ecbd064efa
nacl <vitus.tseng@gmail.com>
2024-05-17 15:15:43 +0800

    fix bug in DocsReader

--- hw4/TFIDFCalculator.java
+++ hw4/TFIDFCalculator.java
@@ -1,6 +1,5 @@
 public class TFIDFCalculator {
     public static void main (String args[]) {
         DocsReader dr = new DocsReader("/home/share/hw4/docs.txt");
-        System.out.println(dr.getDocs());
     }
 }
