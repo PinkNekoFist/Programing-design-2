60eef20bc043460a4441f5181c3f00956801e0df
nacl <vitus.tseng@gmail.com>
2024-05-17 18:53:30 +0800

    add test file

--- hw4/TFIDFCalculator.java
+++ hw4/TFIDFCalculator.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 
 public class TFIDFCalculator {
     public static void main (String args[]) {
-        DocsReader dr = new DocsReader(args[0]);
+        // DocsReader dr = new DocsReader(args[0]);
+        DocsReader dr = new DocsReader("/home/share/hw4/docs.txt");
         
         ArrayList<Trie> wordsInDoc = new ArrayList<>();
         Trie numsOfDocsHasTerm = new Trie();
@@ -14,17 +15,20 @@ public class TFIDFCalculator {
             // s is 5 line doc
             Trie t = new Trie(s);
             wordsInDoc.add(t);
+            // if (t.root != null) System.out.println("t is null");
             numsOfDocsHasTerm.insertToAnother(numsOfDocsHasTerm.root, t.root);
-        }        
+        }
 
         try {
-            File f = new File(args[1]);
+            // File f = new File(args[1]);
+            File f = new File("tc0.txt");
             Scanner scanner = new Scanner(f);
             String terms[] = scanner.nextLine().split(" ");
             String temp[] = scanner.nextLine().split(" ");
             int nums[] = Arrays.stream(temp).mapToInt(Integer::parseInt).toArray();
             scanner.close();
 
+            StringBuilder sb = new StringBuilder();
             for (int i = 0;i < terms.length;i++ ) {
                 System.out.println(tfIdfCalculate(wordsInDoc.get(nums[i]), numsOfDocsHasTerm, terms[i], dr.getDocs().size()));
             }
